87abe396d2204cc46939fc392bc34abc314c367c
NBWMedia0807 <nbwmedia.jp@gmail.com>
2025-09-17 08:22:08 +0900

    Fix Sanity schema imports

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,4 +1,6 @@
-import {defineCliConfig} from 'sanity/cli'
+import {createRequire} from 'module'
+
+const { defineCliConfig } = createRequire(import.meta.url)('sanity/cli')
 
 // Hardcode values for non-interactive deploys
 export default defineCliConfig({
