e2c191af5822893c3d5501a40c40564143d27745
NBWMedia0807 <nbwmedia.jp@gmail.com>
2025-09-18 01:28:32 +0900

    chore: configure vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		files: {
-			appTemplate: 'src/app.html' // This path is relative to the project root
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    files: {
+      appTemplate: 'src/app.html' // This path is relative to the project root
+    }
+  }
 };
 
 export default config;
-
-
