378a545147aa71e63d2092ff12fe026497881fd7
NBWMedia0807 <nbwmedia.jp@gmail.com>
2025-09-18 01:41:09 +0900

    fix: configure vercel adapter for deployment (#140)
    * chore: configure vercel adapter
    
    * fix: align dependencies with lockfile

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,12 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		files: {
-			appTemplate: 'src/app.html' // This path is relative to the project root
-		}
-	}
+       kit: {
+               adapter: adapter(),
+               files: {
+                       appTemplate: 'src/app.html' // This path is relative to the project root
+               }
+       }
 };
 
 export default config;
-
-
