c2864d791f1caf28bb8779d87b82d6987421bc32
NBWMedia0807 <nbwmedia.jp@gmail.com>
2025-10-10 15:21:27 +0900

    fix: expose quiz publishedAt field (#251)

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,7 +1,5 @@
-import {createRequire} from 'module'
+import {defineCliConfig} from 'sanity/cli'
 import { SANITY_DEFAULTS } from '../src/lib/sanityDefaults.js'
-
-const { defineCliConfig } = createRequire(import.meta.url)('sanity/cli')
 
 // Hardcode values for non-interactive deploys
 export default defineCliConfig({
