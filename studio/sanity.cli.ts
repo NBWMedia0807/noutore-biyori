7afdf6cd1e5bcb24cc0e2422b97eec889eaa8ac8
NBWMedia0807 <nbwmedia.jp@gmail.com>
2025-10-26 03:11:34 +0000

    chore(studio): use sanity.cli.ts (ESM) and allow esbuild

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,9 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'quljge22',
+    dataset: 'production',
+  },
+  studioHost: 'noutore-biyori-studio-main',
+})
